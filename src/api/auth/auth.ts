ad9fa0dbd361e0caf5203fda86d2c88a3fb6fc96
김하림 <hrkim0529@naver.com>
2026-01-31 15:37:46 +0900

    feat: auth API 엔드포인트 수정 및 이메일 인증코드 api 함수 생성

--- src/api/auth/auth.ts
+++ src/api/auth/auth.ts
@@ -1,12 +1,35 @@
-import { type EmailLoginResponse, type EmailLoginRequest, type EmailCheckRequest, type EmailCheckResponse } from "../../types/auth";
+import {
+    type EmailLoginResponse,
+    type EmailLoginRequest,
+    type EmailCheckRequest,
+    type EmailCheckResponse,
+    type EmailVerifyRequest,
+    type EmailVerifyResponse,
+    type EmailVerifyCheckRequest,
+    type EmailVerifyCheckResponse
+} from "../../types/auth";
 import { axiosInstance } from "../core/axiosInstance";
 
+// 이메일 로그인
 export const postEmailLogin = async (body: EmailLoginRequest) => {
-    const res = await axiosInstance.post<EmailLoginResponse>("/auth/login/email", body);
+    const res = await axiosInstance.post<EmailLoginResponse>("/api/auth/login/email", body);
     return res.data;
 }
 
+// 이메일 중복 확인
 export const postEmailCheck = async (body: EmailCheckRequest) => {
-    const res = await axiosInstance.post<EmailCheckResponse>("/members/email/check", body);
+    const res = await axiosInstance.post<EmailCheckResponse>("/api/auth/email/check", body);
+    return res.data;
+}
+
+// 이메일 인증코드 발송
+export const postEmailVerifyCodeSend = async (body: EmailVerifyRequest) => {
+    const res = await axiosInstance.post<EmailVerifyResponse>("/api/auth/email/verification-codes", body);
+    return res.data;
+}
+
+// 이메일 인증코드 확인
+export const postEmailVerifyCodeCheck = async (body: EmailVerifyCheckRequest) => {
+    const res = await axiosInstance.post<EmailVerifyCheckResponse>("/api/auth/email/verification-codes/verify", body);
     return res.data;
 }
