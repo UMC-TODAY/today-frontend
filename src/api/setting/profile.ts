8d52dccb4d286b5fa7079e53c4414d27f245e1b6
김하림 <hrkim0529@naver.com>
2026-02-03 23:48:41 +0900

    feat: 프로필 설정 api 타입 정의 및 함수 생성

---
+++ src/api/setting/profile.ts
@@ -0,0 +1,37 @@
+import {
+    type NicknameCheckResponse,
+    type NicknameCheckRequest,
+    type WithdrawResponse,
+    type EditProfileRequest,
+    type EditProfileResponse,
+    type MyInfoResponse
+} from "../../types/profile";
+import { axiosInstance } from "../core/axiosInstance";
+
+// 닉네임 중복 확인
+export const postNicknameCheck = async (body: NicknameCheckRequest) => {
+    const res = await axiosInstance.post<NicknameCheckResponse>("/api/members/nickname/check", body);
+    return res.data;
+}
+
+// 회원 탈퇴
+export const patchWithdraw = async (token: string) => {
+    const res = await axiosInstance.patch<WithdrawResponse>("/api/members/withdraw", {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    });
+    return res.data;
+}
+
+// 프로필 정보 수정
+export const patchEditProfile = async (body: EditProfileRequest) => {
+    const res = await axiosInstance.patch<EditProfileResponse>("/api/members/profile", body);
+    return res.data;
+}
+
+// 내 정보 보기
+export const getMyInfo = async () => {
+    const res = await axiosInstance.get<MyInfoResponse>("/api/members/me");
+    return res.data;
+}
